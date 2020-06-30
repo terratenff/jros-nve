6ceb2449791503efb763651607ff2a0fdb6f8de4
terratenff <49789537+terratenff@users.noreply.github.com>
2020-06-30 16:20:27 +0300

    Implemented prompts

--- src/main/java/org/tt/indproj/core/story/prompt/Prompt.java
+++ src/main/java/org/tt/indproj/core/story/prompt/Prompt.java
@@ -20,22 +20,22 @@ class Prompt implements IPrompt {
 	/**
 	 * List of positions in the story that the prompt is present in.
 	 */
-	private List<Integer> positions = new ArrayList<Integer>();
+	private List<Integer> positions;
 	
 	/**
 	 * Description of the prompt: assists the user both contextually and grammatically.
 	 */
-	private String description = "";
+	private String description;
 	
 	/**
 	 * Default prompt set for the story if the user does not set it.
 	 */
-	private String defaultPrompt = "";
+	private String defaultPrompt;
 	
 	/**
 	 * Prompt set by the user.
 	 */
-	private String prompt = "";
+	private String prompt;
 	
 	/**
 	 * Constructor function for new prompts. Note that an ID has to be assigned
@@ -45,9 +45,14 @@ class Prompt implements IPrompt {
 	 * @param description Description set to help the user provide a prompt.
 	 * @param defaultPrompt A prompt that is used by default if the user does not
 	 * provide one.
+	 * @param prompt A prompt provided by the user. An empty string is treated
+	 * as if the user did not provide one.
 	 */
-	Prompt(List<Integer> positions, String description, String defaultPrompt) {
-		// TODO
+	Prompt(List<Integer> positions, String description, String defaultPrompt, String prompt) {
+		this.positions = positions;
+		this.description = description;
+		this.defaultPrompt = defaultPrompt;
+		this.prompt = prompt;
 	}
 	
 	/**
@@ -57,13 +62,27 @@ class Prompt implements IPrompt {
 	 * Multiple word indexes are separated by commas.
 	 */
 	Prompt(String csv) {
-		// TODO
+		String[] components = csv.split(";");
+		String csvId = components[0];
+		String csvIndexes = components[1];
+		String csvDescription = components[2];
+		String csvDefault = components[3];
+		if (components.length < 5) prompt = "";
+		else prompt = components[4];
+		
+		id = Integer.parseInt(csvId);
+		String[] wordIndexes = csvIndexes.split(",");
+		positions = new ArrayList<Integer>();
+		for (String index : wordIndexes) {
+			positions.add(Integer.parseInt(index));
+		}
+		description = csvDescription;
+		defaultPrompt = csvDefault;
 	}
 
 	@Override
 	public int getId() {
-		// TODO Auto-generated method stub
-		return 0;
+		return id;
 	}
 	
 	@Override
@@ -73,32 +92,38 @@ class Prompt implements IPrompt {
 
 	@Override
 	public List<Integer> getPositions() {
-		// TODO Auto-generated method stub
-		return null;
+		return positions;
 	}
 
 	@Override
 	public String getDescription() {
-		// TODO Auto-generated method stub
-		return null;
+		return description;
 	}
 
 	@Override
 	public String getDefault() {
-		// TODO Auto-generated method stub
-		return null;
+		return defaultPrompt;
 	}
 
 	@Override
 	public String getPrompt() {
-		// TODO Auto-generated method stub
-		return null;
+		return prompt;
 	}
 
 	@Override
 	public String toString() {
-		// TODO: promptId;wordIndexes;promptDescription;promptDefault;promptFilled
+		String str = "";
+		str += id + ";";
+		for (int i = 0; i < positions.size(); i++) {
+			int index = positions.get(i);
+			if (i == positions.size() - 1) str += index + ";";
+			else str += index + ",";
+		}
+		str += description + ";";
+		str += defaultPrompt + ";";
+		str += prompt;
+		// Result: promptId;wordIndexes;promptDescription;promptDefault;promptFilled
 		// Multiple word indexes are separated with commas (",").
-		return "";
+		return str;
 	}
 }
